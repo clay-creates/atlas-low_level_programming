1183902bf64196e37ce0cceaf1811211fa0ac60e
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-10 23:01:37 +0000

    give return

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -9,9 +9,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
+    size_t counter = 0;
+
     while (h != NULL)
     {
         printf("%d\n", h->n);
+        counter++;
         h = h->next;
     }
+    return (counter);
 }
