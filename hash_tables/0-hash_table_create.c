f01a7b9c274e8e4b1fb6a679af368177d24abbae
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-11 00:02:06 +0000

    edit 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,6 +11,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *newTable = NULL;
 
+    if (size == 0)
+    {
+        return (NULL);
+    }
+
     newTable = malloc(sizeof(hash_table_t));
 
     if (newTable == NULL)
@@ -18,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
         return (NULL);
     }
 
-    newTable->array - calloc(size, sizeof(hash_node_t *));
+    newTable->array = calloc(size, sizeof(hash_node_t *));
 
     if (newTable->array == NULL)
     {
