0a3eed05e9f5a8f6f4bebda95d7cbd42c3f12b2f
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-11 15:48:01 +0000

    task 1

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,5 +1,21 @@
 #include "hash_tables.h"
 
 /**
+ * hash_djb2 - Hash function implementing Dbj2 algorithm
+ * @str: String to be translated to hash value
  *
+ * Return: Returns the translated hash value
  */
+
+unsigned long int hash_dbj2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
