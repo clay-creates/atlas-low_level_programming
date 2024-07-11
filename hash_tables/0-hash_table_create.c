43b45367006034d8b00b14b7c892588fa3c362d8
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-11 00:00:46 +0000

    make files

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array to be created
+ *
+ * Return: Returns a pointer to new hash table on success or NULL on fail
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *newTable = NULL;
+
+    newTable = malloc(sizeof(hash_table_t));
+
+    if (newTable == NULL)
+    {
+        return (NULL);
+    }
+
+    newTable->array - calloc(size, sizeof(hash_node_t *));
+
+    if (newTable->array == NULL)
+    {
+        free(newTable);
+        return (NULL);
+    }
+
+    newTable->size = size;
+
+    return (newTable);
+}
---
+++ hash_tables/README.md
@@ -0,0 +1,1 @@
+# Atlas Low Level Programming - Hash Tables
