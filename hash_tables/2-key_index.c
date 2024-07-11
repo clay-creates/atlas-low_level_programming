0434f26a039c8dfd78ab1048d764577cfdd7fef7
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-11 15:52:38 +0000

    task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,19 @@
 #include "hash_tables.h"
 
 /**
+ * key_index - Gives the index of a specified key value
+ * @key: The key to search for
+ * @size: Size of the hash table array
  *
+ * Return: Returns the index of key/value pair
  */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hashValue, keyReturn;
+
+	hashValue = hash_djb2(key);
+	keyReturn = hashValue % size;
+
+	return (keyReturn);
+}
