7470c1119475c42006b10c6286eb0e1d85ab0193
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-13 20:08:59 +0000

    indentation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,44 +11,44 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *newNode, *currentNode;
+	unsigned long int index;
+	hash_node_t *newNode, *currentNode;
 
-    if (ht == NULL || key == NULL || value == NULL)
-    {
-        return (0);
-    }
-    index = key_index((const unsigned char *)key, ht->size);
-    currentNode = ht->array[index];
-    while (currentNode != NULL)
-    {
-        if (strcmp(currentNode->key, key) == 0)
-        {
-            free(currentNode->value);
-            currentNode->value = strdup(value);
-            return (1);
-        }
-        currentNode = currentNode->next;
-    }
-    newNode = malloc(sizeof(hash_node_t));
-    if (newNode == NULL)
-    {
-        return (0);
-    }
-    newNode->key = strdup(key);
-    if (newNode->key == NULL)
-    {
-        free(newNode);
-        return (0);
-    }
-    newNode->value = strdup(value);
-    if (newNode->value == NULL)
-    {
-        free(newNode->key);
-        free(newNode);
-        return (0);
-    }
-    newNode->next = ht->array[index];
-    ht->array[index] = newNode;
-    return (1);
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
+	currentNode = ht->array[index];
+	while (currentNode != NULL)
+	{
+		if (strcmp(currentNode->key, key) == 0)
+		{
+			free(currentNode->value);
+			currentNode->value = strdup(value);
+			return (1);
+		}
+		currentNode = currentNode->next;
+	}
+	newNode = malloc(sizeof(hash_node_t));
+	if (newNode == NULL)
+	{
+		return (0);
+	}
+	newNode->key = strdup(key);
+	if (newNode->key == NULL)
+	{
+		free(newNode);
+		return (0);
+	}
+	newNode->value = strdup(value);
+	if (newNode->value == NULL)
+	{
+		free(newNode->key);
+		free(newNode);
+		return (0);
+	}
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
+	return (1);
 }
