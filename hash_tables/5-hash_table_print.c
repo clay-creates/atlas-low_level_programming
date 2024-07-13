8406337872c4d60d4ff062fcff30cacedc953a65
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-13 20:56:23 +0000

    indentation

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,29 +7,29 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *currentNode;
-    int first = 1;
+	unsigned long int i;
+	hash_node_t *currentNode;
+	int first = 1;
 
-    if (ht == NULL)
-    {
-        return;
-    }
+	if (ht == NULL)
+	{
+		return;
+	}
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        currentNode = ht->array[i];
-        while (currentNode != NULL)
-        {
-            if (first == 0)
-            {
-                printf(", ");
-            }
-            printf("'%s': '%s'", currentNode->key, currentNode->value);
-            currentNode = currentNode->next;
-            first = 0;
-        }
-    }
-    printf("}\n");
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		currentNode = ht->array[i];
+		while (currentNode != NULL)
+		{
+			if (first == 0)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", currentNode->key, currentNode->value);
+			currentNode = currentNode->next;
+			first = 0;
+		}
+	}
+	printf("}\n");
 }
