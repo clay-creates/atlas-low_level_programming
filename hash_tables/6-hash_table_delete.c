75f4d1bf5662990937e798c3c6351cf4d3f3870b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-13 21:00:13 +0000

    task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,5 +1,32 @@
 #include "hash_tables.h"
 
 /**
- *
+ * hash_table_delete - Deletes a hash table
+ * @ht: Hash table to be deleted
  */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *currentNode, *temp;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		currentNode = ht->array[i];
+		while (currentNode != NULL)
+		{
+			temp = currentNode;
+			currentNode = currentNode->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
