904e0ed932223a8b6f3a4fad8dcc7cf8b89d9ad3
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-13 20:55:23 +0000

    task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,5 +1,35 @@
 #include "hash_tables.h"
 
 /**
- *
+ * hash_table_print - Prints a hash table to standard output
+ * @ht: Hash table to be printed
  */
+
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *currentNode;
+    int first = 1;
+
+    if (ht == NULL)
+    {
+        return;
+    }
+
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        currentNode = ht->array[i];
+        while (currentNode != NULL)
+        {
+            if (first == 0)
+            {
+                printf(", ");
+            }
+            printf("'%s': '%s'", currentNode->key, currentNode->value);
+            currentNode = currentNode->next;
+            first = 0;
+        }
+    }
+    printf("}\n");
+}
