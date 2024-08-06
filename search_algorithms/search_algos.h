e779901d3e26055f3066155a16389f4ae6dd5860
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-06 19:56:08 +0000

    update header with binary search method

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,5 +6,6 @@
 
 int linear_search(int *array, size_t size, int value);
 void print_array(int *array, size_t start, size_t end);
+int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
