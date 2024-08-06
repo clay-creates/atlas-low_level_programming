220747af8652341193da8f6c763108b304867932
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-06 19:56:23 +0000

    binary search implemention

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,49 @@
+#include "search_algos.h"
+
+void print_array(int *array, size_t start, size_t end)
+{
+    size_t i;
+
+    printf("Searching in array: ");
+    for (i = start; i <= end; i++)
+    {
+        printf("%d", array[i]);
+        if (i < end)
+        {
+            printf(", ");
+        }
+    }
+    printf("\n");
+}
+
+int binary_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+    {
+        return (-1);
+    }
+
+    size_t left = 0;
+    size_t right = size - 1;
+    size_t mid;
+
+    while (left <= right)
+    {
+        print_array(array, left, right);
+        mid = left + (right - left) / 2;
+
+        if (array[mid] == value)
+        {
+            return (mid);
+        }
+        else if (array[mid] < value)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return (-1);
+}
