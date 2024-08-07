f25e60a338a6233efd2fa0756c8a76ea0a0d209c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-07 21:48:47 +0000

    fix print with space

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array [%u] = [%d]\n", (unsigned int)i, array[i]);
+		printf("Value checked array[%u] = [%d]\n", (unsigned int)i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
