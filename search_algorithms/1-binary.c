a21899b8d38b5689e49870d27aeca5beac6e4c9e
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-08-07 21:38:57 +0000

    cast unsigned int, indentation

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -2,48 +2,48 @@
 
 void print_array(int *array, size_t start, size_t end)
 {
-    size_t i;
+	size_t i;
 
-    printf("Searching in array: ");
-    for (i = start; i <= end; i++)
-    {
-        printf("%d", array[i]);
-        if (i < end)
-        {
-            printf(", ");
-        }
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		printf("%d", array[i]);
+		if (i < end)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-    while (left <= right)
-    {
-        print_array(array, left, right);
-        mid = left + (right - left) / 2;
+	while (left <= right)
+	{
+		print_array(array, left, right);
+		mid = left + (right - left) / 2;
 
-        if (array[mid] == value)
-        {
-            return (mid);
-        }
-        else if (array[mid] < value)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-    }
-    return (-1);
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+	return (-1);
 }
